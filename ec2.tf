8a3bc76520b4277d8e01d95ae5a5badd387a8a27
kprasanth999 <85720077+kprasanth999@users.noreply.github.com>
2022-01-25 09:28:38 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,9 +10,5 @@ resource "aws_instance" "ec2" {
   ami             = var.ec2_ami_id[var.region]
   instance_type   = var.instance_type
   subnet_id       = local.pub_sub_ids[count.index]
-  user_data       = file("scripts/apache.sh")
-  vpc_security_group_ids = [aws_security_group.ec2-sg.id] 
-  key_name        = aws_key_pair.key.key_name
-  iam_instance_profile = aws_iam_instance_profile.s3_ec2_profile.name
   tags            = local.web_tags
 }
